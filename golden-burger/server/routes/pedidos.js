dfccdcc9fa35d7645f33f4126dddf3b81a344c07
Felipe.Tavella <felipe.tavella100@gmail.com>
2026-09-02 00:43:45 -0300

    Delete golden-burger directory

--- golden-burger/server/routes/pedidos.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ctrl = require('../controllers/pedidos.controller');
-
-router.post('/', ctrl.criar);
-router.get('/', ctrl.listar);
-router.patch('/:id/status', ctrl.atualizarStatus);
-
-module.exports = router;
